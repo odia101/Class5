908b3044d4b4dc7762bf552bc394985047f3b3fd
odia101 <odia.akioya@gmail.com>
2019-02-16 09:17:45 -0500

    New file

---
+++ generic_parser.py
@@ -0,0 +1,7 @@
+#1. Load a dataset from a file
+#2. "organize" that file, so we can access columns *or* rows of it easily
+#3. Compute some "summary statistics" about the dataset
+#4. print those summary statistics
+
+
+#1. Load a dataset
